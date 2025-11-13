fbfd5b73b8ee587d4cb841fee2d525f560fc740c
ahmedsdiri7 <ahmed.sdiri@esprit.tn>
2025-11-13 22:36:53 +0100

    Migration Angular + nettoyage des dépendances et configuration frontend

--- ngx-admin/src/app/pages/modal-overlays/modal-overlays.module.ts
+++ ngx-admin/src/app/pages/modal-overlays/modal-overlays.module.ts
@@ -76,9 +76,8 @@ const MODULES = [
 const SERVICES = [];
 
 @NgModule({
-  imports: [...MODULES],
-  declarations: [...COMPONENTS],
-  providers: [...SERVICES],
-  entryComponents: [...ENTRY_COMPONENTS],
+    imports: [...MODULES],
+    declarations: [...COMPONENTS],
+    providers: [...SERVICES]
 })
 export class ModalOverlaysModule {}
